34dc0f8b6a53e82d4ee433b95dba245409a53c96
Antonio Hollerman <toniohollerprof@gmail.com>
2023-08-06 00:27:15 -0400

    Created a repo for my calendar project.

---
+++ sqlite_ver/calendar_manager_sqlite.py
@@ -0,0 +1,12 @@
+from window_classes import *
+
+window = Calendar()
+window.run()
+
+if window.current_window == "Schedule Frame":
+    save_changes(window.schedule_frame.info_row_container)
+
+cal_conn.commit()
+cal_cur.close()
+cal_conn.cursor()
+# Python -m PyInstaller calendar_manager_sqlite.py --windowed --onefile
